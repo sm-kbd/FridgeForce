2e00a2b5811ce51f955544f991eab37d23073c20
0P01003 <kd1370025@st.kobedenshi.ac.jp>
2025-06-09 11:07:24 +0900

    ヘッダーの色変更

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
                     home: Scaffold(
                         appBar: AppBar(
                             title: const Text("FridgeForce"),
-                            backgroundColor: Colors.green,
+                            backgroundColor: Colors.red,
                             ),
 
                         body: ListView(
