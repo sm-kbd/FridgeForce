a668b37e435fbeb0ae0a5918a4610058e47e6031
sm-kbd <kd1404385@st.kobedenshi.ac.jp>
2025-06-09 11:18:00 +0900

    base scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -17,138 +17,24 @@ class MyApp extends StatelessWidget {
                             backgroundColor: Colors.green,
                             ),
 
-                        body: ListView(
-                            padding: const EdgeInsets.only(top: 100, bottom: 100, left: 20, right: 20),
-                            shrinkWrap: true,
-                            children: [
-                            Container(
-                            child: Row(
-                                children: [
-                                Container(
-                                    height: 100,
-                                    width: 100,
-                                    decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-                                    child: IconButton(
-                                        onPressed: () {},
-                                        icon: Icon(Icons.photo_camera)
-                                        ),
-                                    ),
-                                Expanded(
-                                    child: Container(
-                                        padding: EdgeInsets.only(left: 20),
-                                        child: TextField(
-                                            decoration: const InputDecoration(
-                                                label: Text("食材名"),
-                                                ),
-                                            ),
-                                        ),
-                                    ),
-                                ],
+                        bottomNavigationBar: BottomNavigationBar(
+                            type: BottomNavigationBarType.fixed,
+                            items: const [
+                            BottomNavigationBarItem(
+                                icon: Icon(Icons.density_small_sharp),
+                                label: "リストアップ",
                                 ),
+                            BottomNavigationBarItem(
+                                icon: Icon(Icons.add),
+                                label: "登録",
                                 ),
-                            Container(
-                            child: Row(
-                                children: [
-                                Expanded(
-                                    child: Container(
-                                        padding: EdgeInsets.only(left: 20),
-                                        child: TextField(
-                                            decoration: const InputDecoration(
-                                                label: Text("年"),
-                                                ),
-                                            ),
-                                        ),
-                                    ),
-                                Expanded(
-                                    child: Container(
-                                        padding: EdgeInsets.only(left: 20),
-                                        child: TextField(
-                                            decoration: const InputDecoration(
-                                                label: Text("月"),
-                                                ),
-                                            ),
-                                        ),
-                                    ),
-                                Expanded(
-                                    child: Container(
-                                        padding: EdgeInsets.only(left: 20),
-                                        child: TextField(
-                                            decoration: const InputDecoration(
-                                                label: Text("日"),
-                                                ),
-                                            ),
-                                        ),
-                                    ),
-                                ],
+                            BottomNavigationBarItem(
+                                icon: Icon(Icons.pie_chart),
+                                label: "チャート表示",
                                 ),
-                                ),
-                            Container(
-                            child: Row(
-                                children: [
-                                Expanded(
-                                    child: Container(
-                                        padding: EdgeInsets.only(left: 20),
-                                        child: TextField(
-                                            decoration: const InputDecoration(
-                                                label: Text("カテゴリー"),
-                                                ),
-                                            ),
-                                        ),
-                                    ),
-                                Container(
-                                        width: 130,
-                                        padding: EdgeInsets.only(left: 20, top: 20),
-                                        child: SpinBox(
-                                            min: 1,
-                                            max: 100,
-                                            value: 1,
-                                            onChanged: (value) => print(value),
-                                        ),
-                                    ),
-                                ],
-                                ),
-                                ),
-                            Container(
-                            child: Row(
-                                children: [
-                                Expanded(
-                                    child: Container(
-                                        padding: EdgeInsets.only(left: 20),
-                                        child: TextField(
-                                            decoration: const InputDecoration(
-                                                label: Text("メモ"),
-                                                ),
-                                            ),
-                                        ),
-                                    ),
-                                ],
-                                ),
-                                ),
-                                ],
-                                ),
-
-                                bottomNavigationBar: BottomNavigationBar(
-                                        type: BottomNavigationBarType.fixed,
-                                        items: const [
-                                        BottomNavigationBarItem(
-                                            icon: Icon(Icons.density_small_sharp),
-                                            label: "リストアップ",
-                                            ),
-                                        BottomNavigationBarItem(
-                                            icon: Icon(Icons.photo_camera),
-                                            label: "画像登録",
-                                            ),
-                                        BottomNavigationBarItem(
-                                            icon: Icon(Icons.add),
-                                            label: "登録",
-                                            ),
-                                        BottomNavigationBarItem(
-                                            icon: Icon(Icons.pie_chart),
-                                            label: "チャート表示",
-                                            ),
-                                        ],
-                                        ),
-                                        ),
-                                        );
+                            ],
+                            ),
+                            ),
+                            );
         }
 }
