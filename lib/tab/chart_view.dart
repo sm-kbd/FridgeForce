fe7d48cd0179396922877a36593761d10f68562e
sm-kbd <kd1404385@st.kobedenshi.ac.jp>
2025-07-18 13:05:49 +0900

    fixed warningggg

--- lib/tab/chart_view.dart
+++ lib/tab/chart_view.dart
@@ -19,10 +19,6 @@ class ChartsView extends StatelessWidget {
 class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('う'),
-      ),
-    );
+    return const Scaffold(body: Center(child: Text('う')));
   }
 }
